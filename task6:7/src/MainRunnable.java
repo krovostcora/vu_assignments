53a1217b0383f9277caee39a4606bd23bcbb8360
krovostcora <58918909+krovostcora@users.noreply.github.com>
2024-12-08 08:57:02 +0100

    Delete task6:7 directory

--- task6:7/src/MainRunnable.java
+++
@@ -1,8 +0,0 @@
-public class MainRunnable {
-    public static void main(String[] args) {
-        Account account = new Account(2024);
-        for (int i = 0; i < 5; i++) {
-            new Thread(new ChangeBalanceRunnable(account)).start();
-        }
-    }
-}
--- task6:7/src/MainThread.java
+++
@@ -1,9 +0,0 @@
-public class MainThread {
-    public static void main(String[] args) {
-        Account account = new Account(4444);
-        for (int i = 0; i < 5; i++) {
-            new ChangeBalance(account).start();
-        }
-    }
-}
-
