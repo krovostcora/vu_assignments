5acd01844fdb7c1a5797e91cd35b9802302b6cfe
krovostcora <58918909+krovostcora@users.noreply.github.com>
2025-02-13 00:35:40 +0100

    Add files via upload

---
+++ Component & Parallel OP/Lw_C2_Weather/TemperatureSensorServer/TemperatureSensorServer.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TemperatureSensorServer
+{
+    static void Main()
+    {
+        var listener = new TcpListener(IPAddress.Any, 333);
+        listener.Start();
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            var client = listener.AcceptTcpClient();
+            var stream = client.GetStream();
+            
+            for (int i = 0; i < 5; i++)
+            {
+                var temperatureData = GetTemperatureData();
+                var data = Encoding.ASCII.GetBytes(temperatureData.ToString());
+                stream.Write(data, 0, data.Length);
+                System.Threading.Thread.Sleep(1000); 
+            }
+
+            client.Close();
+        }
+    }
+
+    static double GetTemperatureData()
+    {
+        Random generator = new Random();
+        return generator.Next(-40, 45); 
+    }
+}
---
+++ Component & Parallel OP/Lw_C2_Weather/WeatherForecastClient/WeatherForecastClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Sockets;
+using System.Text;
+
+class WeatherForecastClient
+{
+    static void Main()
+    {
+        var client = new TcpClient("localhost", 333);
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
+        var bytesRead = stream.Read(buffer, 0, buffer.Length);
+        var temperatureData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        
+        // Define English culture to ensure English day names
+        var englishCulture = new CultureInfo("en-US");
+        
+        Console.WriteLine($"Temperature for: ");
+        // Print temperature for each day
+        for (int i = 0; i < 5; i++)
+        {
+            // Calculate the date (Today, Tomorrow, etc.)
+            DateTime currentDate = DateTime.Today.AddDays(i);
+            string formattedDate = currentDate.ToString("dddd, dd/MM", englishCulture);
+            Console.WriteLine($"{formattedDate}: {temperatureData}°C");
+        }
+        client.Close();
+    }
+}
